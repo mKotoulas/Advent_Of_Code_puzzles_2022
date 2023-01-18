518eb827acb54a64579c11528bbd43f2b3ae8679
M.K <40547620+mKotoulas@users.noreply.github.com>
2023-01-18 22:50:26 +0200

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -1,11 +1,11 @@
-def get_input():
+def get_input()->list[list[int]]:
     with open("input.txt","r") as f:
         return [[int(food) for food in foods.split("\n")] for foods in f.read().split("\n\n")]
 
-def top_elf(foods:list[list[int]]):
+def top_elf(foods:list[list[int]])->int:
     return sorted([sum(calories) for calories in foods],reverse=True)[0]
 
-def top_three_elves(foods:list[list[int]]): 
+def top_three_elves(foods:list[list[int]])->int: 
     return sum(sorted([sum(calories) for calories in foods],reverse=True)[:3])
     
 print(f"Top elf is carrying in total:{top_elf(get_input())} calories")
